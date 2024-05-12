a7cf1c4348c2cb79a0accc9bf6c1c0527133ad19
sarveshmore2004 <sarveshmore160404@gmail.com>
2024-05-12 13:58:36 +0530

    search added

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
-
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found!");
+  };
   return (
     <div>
-      <form className=" h-20 flex p-4 items-center gap-3 pb-0 mb-0">
+      <form
+        onSubmit={handleSubmit}
+        className=" h-20 flex p-4 items-center gap-3 pb-0 mb-0"
+      >
         <input
           type="text"
           placeholder="Search…"
           className=" bg-cust_light flex-grow input input-bordered rounded-full min-w-10"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <button
           type="submit"
