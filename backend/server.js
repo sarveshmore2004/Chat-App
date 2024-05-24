ccaee331642d88239c6422a1ab627e039b237c92
sarveshmore2004 <sarveshmore160404@gmail.com>
2024-05-24 21:08:01 +0530

    ready for deployement v2

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
@@ -12,7 +13,10 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +28,11 @@ app.use("/api/users", userRoutes);
 // app.get('/' , (req,res)=>{
 //     res.send('HELLOOO WORLLLD');
 // })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, async () => {
   await connectToMongoDB();
